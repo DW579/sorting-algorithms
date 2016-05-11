f5c7d5d4445e1977c27c3612733de4a51b32e7f6
Matt Lane <lane.matthew@gmail.com>
2016-05-11 11:56:39 -0700

    restructure tests

--- test/sortingAlgorithmsSpecPart1.js
+++ test/sortingAlgorithmsSpecPart1.js
@@ -4,6 +4,18 @@ let expect = require('chai').expect
 
 describe("Part 1", function() {
 
+  describe("#swap", function() {
+
+    let swap = require("../algorithms/helpers").swap;
+    
+    it("swaps to elements in an array", function() {
+      let arr = [6, 7, 4, 3, 1];
+      swap(arr, 1, 4);
+      expect(arr).to.deep.equal([6, 1, 4, 3, 7]);
+    });
+  
+  });
+
   describe("#bubbleSort", function(){
 
     let bubbleSort = require("../algorithms/sortingAlgorithmsPart1").bubbleSort;
@@ -66,54 +78,6 @@ describe("Part 1", function() {
 
     it("works with arrays that are already sorted", function(){
       expect(insertionSort([1,2,3,4,5])).to.deep.equal([1,2,3,4,5]);
-    });
-
-  });
-
-});
-
-describe("Part 2", function() {
-
-  describe("#mergeSort", function(){
-
-    let mergeSort = require("../algorithms/sortingAlgorithmsPart2").mergeSort;
-
-    it("sorts an array of positive numbers", function(){
-      expect(mergeSort([3.1,4.5,1,2,5])).to.deep.equal([1,2,3.1,4.5,5]);
-    });
-
-    it("sorts an array of negative numbers", function(){
-      expect(mergeSort([-4,-9,-2,-8,-2,0])).to.deep.equal([-9,-8,-4,-2,-2,0]);
-    });
-
-    it("sorts an array of positive and negative numbers", function(){
-      expect(mergeSort([-3,4,1,-9,22])).to.deep.equal([-9,-3,1,4,22]);
-    });
-
-    it("works with arrays that are already sorted", function(){
-      expect(mergeSort([1,2,3,4,5])).to.deep.equal([1,2,3,4,5]);
-    });
-
-  });
-
-  describe("#quickSort", function(){
-
-    let quickSort = require("../algorithms/sortingAlgorithmsPart2").quickSort;
-
-    it("sorts an array of positive numbers", function(){
-      expect(quickSort([3.1,4.5,1,2,5])).to.deep.equal([1,2,3.1,4.5,5]);
-    });
-
-    it("sorts an array of negative numbers", function(){
-      expect(quickSort([-4,-9,-2,-8,-2,0])).to.deep.equal([-9,-8,-4,-2,-2,0]);
-    });
-
-    it("sorts an array of positive and negative numbers", function(){
-      expect(quickSort([-3,4,1,-9,22])).to.deep.equal([-9,-3,1,4,22]);
-    });
-
-    it("works with arrays that are already sorted", function(){
-      expect(quickSort([1,2,3,4,5])).to.deep.equal([1,2,3,4,5]);
     });
 
   });
