3a9e9b26d24ca76ea17ee214d8a873d3bb322594
Matt Lane <lane.matthew@gmail.com>
2016-05-15 22:24:19 -0700

    quicksort solutions

--- algorithms/sortingAlgorithmsPart2.js
+++ algorithms/sortingAlgorithmsPart2.js
@@ -8,8 +8,15 @@ function mergeSort(arr) {
 
 }
 
-function quickSort(arr) {
+function quickSort(arr, left=0, right=arr.length - 1){
 
+  if(left < right){
+   var partitionIndex = partition(arr, left, right);
+   quickSort(arr, left, partitionIndex - 1);
+   quickSort(arr, partitionIndex + 1, right);
+  }
+  
+  return arr;
 }
 
 module.exports = {
